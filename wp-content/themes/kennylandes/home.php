bed7762af97fe6179a3c200800539407364f053c
Kenny Landes <kplandes@gmail.com>
2013-02-18 15:09:25 -0800

    Changes to theme.

--- wp-content/themes/kennylandes/home.php
+++ wp-content/themes/kennylandes/home.php
@@ -10,36 +10,8 @@
     <div class="row">
         <div class="span4">
             <h2>Nike+</h2>
-				<h3>My run totals</h3>
-				<?php $args = array(
-		            'runner'       => '1', // accepts a single user_id
-		            'show_list'     => true,
-		            'show_name'     => false,
-		            'show_runs'     => true,
-		            'show_distance' => true,
-		            'show_duration' => true,
-		            'show_pace'     => true,
-		            'show_fuel'     => true,
-		            'show_calories' => true,
-					);
-				echo nikeplus_individual_totals( $args ); ?>
-				<h3>My personal bests</h3>
-				<?php $args = array(
-			        'runner'		=> '1', // accepts a single user_id
-			        'show_list'     => true,
-			        'show_name'     => false,
-			        'show_1k'       => false,
-			        'show_1m'       => true,
-			        'show_5k'       => true,
-			        'show_10k'      => true,
-			        'show_half'     => true,
-			        'show_full'     => true,
-			        'show_farthest' => true,
-			        'show_longest'  => true,
-			        'show_calories' => true
-			    );
-			    echo nikeplus_personal_records( $args ); ?>
-            <p><a class="btn" href="http://nikeplus.nike.com/plus/profile/kplandes/">View details &raquo;</a></p>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p><a class="btn" href="#">View details &raquo;</a></p>
         </div>
         <div class="span4">
             <h2>Facebook</h2>
