4300a3d07206bea8c3fc13d282aac0fde3b61225
Kenny Landes <kplandes@gmail.com>
2013-04-14 23:38:35 -0700

    Added home page post number to header.php to exclude from site nav.

--- wp-content/themes/kennylandes/header.php
+++ wp-content/themes/kennylandes/header.php
@@ -106,7 +106,7 @@
 
         <!-- Navigation -->
         <ul id="navigation">
-        	<?php wp_list_pages("title_li=&depth=2&exclude=948"); ?>
+        	<?php wp_list_pages("title_li=&depth=2&exclude=948, 877"); ?>
         </ul><!-- /Navigation -->
 
     </div><!-- /Header -->
