39c18f21e1ed9cbb8c7c11893a5af5742bdbd351
Kenny Landes <kplandes@gmail.com>
2013-06-27 15:06:01 -0700

    Reworking commented out code, artifacts displaying on page.

--- wp-content/themes/kennylandes/front-page.php
+++ wp-content/themes/kennylandes/front-page.php
@@ -34,7 +34,7 @@
 					<div class="caption lft big_orange"  data-x="506" data-y="157" data-speed="400" data-start="1900" data-easing="easeOutExpo">It's a responsive world now, sites must adapting</div>
 					<div class="caption lfr medium_grey"  data-x="510" data-y="230" data-speed="300" data-start="2500" data-easing="easeOutExpo">Specification</div>
 	                <div class="caption lfr small_text"  data-x="510" data-y="260" data-speed="300" data-start="2600" data-easing="easeOutExpo">Contrary to popular belief Lorem Ipsum is not simply random text. It has roots<br> in a piece of classical Latin literature from 45 BC making it over 2000 years old.<br> Richard McClintock a latin professor at Hampden-Sydney College in Virginia <br>looked up one of the more obscure words.</div>
-	                <!-- <div class="caption sfb" data-x="510" data-y="360" data-speed="1000" data-start="1800" data-easing="easeOutBack"><a href="#" class="btn-image">Read more</a></div> -->
+	                <div class="caption sfb" data-x="510" data-y="360" data-speed="1000" data-start="1800" data-easing="easeOutBack"><a href="#" class="btn-image">Read more</a></div>
 				</li>				
 -->
 			</ul>
