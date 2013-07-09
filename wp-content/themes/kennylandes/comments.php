08d6d60e27ab4df26762b4e0ee68cd46a127bb13
Kenny Landes <kplandes@gmail.com>
2013-07-09 10:26:17 -0700

    Fixed comments submit button for now.

--- wp-content/themes/kennylandes/comments.php
+++ wp-content/themes/kennylandes/comments.php
@@ -15,8 +15,8 @@
 					<?php echo get_avatar( $comment, 60 ); ?>
 	                <div class="comment-meta">
 	                    <h6 class="author">
-	                    	<a href="#"><?php echo $comment->comment_author ?></a> 
-	                    	<span class="date"><?php comment_date('F j, Y', $comment->comment_ID) ?></span> 
+	                    	<a href="#"><?php echo $comment->comment_author ?></a>
+	                    	<span class="date"><?php comment_date('F j, Y', $comment->comment_ID) ?></span>
 	                    	<!-- <a href="#" class="reply">Reply</a> -->
 	                	</h6>
 	                </div>
@@ -31,22 +31,28 @@
 		<?php endforeach; ?>
 	    </ol>
 	<?php endif; ?>
-    
+
     <!-- Comments Form -->
-	<?php if (comments_open()): ?>  
+	<?php if (comments_open()): ?>
     <h4>Add a Comment</h4>
     <form method="post" id="comments-form" action="<?php echo site_url( '/wp-comments-post.php' ); ?>">
 		<?php comment_id_fields($id); wp_comment_form_unfiltered_html_nonce(); ?>
 		<?php if (!is_user_logged_in()): ?>
-	        <input type="text" onfocus="if(this.value == 'Name') { this.value = ''; }" onblur="if(this.value == '') { this.value = 'Name'; }" value="Name" >
-	        <input type="text" onfocus="if(this.value == 'E-mail') { this.value = ''; }" onblur="if(this.value == '') { this.value = 'E-mail'; }" value="E-mail" >
-	        <input type="text" onfocus="if(this.value == 'Website') { this.value = ''; }" onblur="if(this.value == '') { this.value = 'Website'; }" value="Website" class="last" >
+	        <div class="name">
+                <input class="text" name="author" size="30" value="Name" onFocus="if(this.value == 'Name') {this.value = '';}" onBlur="if (this.value == '') {this.value = 'Name';}" />
+            </div>
+            <div class="email">
+                <input class="text" name="email" size="30" value="Email" onFocus="if(this.value == 'Email') {this.value = '';}" onBlur="if (this.value == '') {this.value = 'Email';}" />
+            </div>
+            <div class="website">
+                <input class="text last" name="website" size="30" value="Website" onFocus="if(this.value == 'Website') {this.value = '';}" onBlur="if (this.value == '') {this.value = 'Website';}" />
+            </div>
 		<?php endif; ?>
-        <textarea cols="88" rows="12" onfocus="if(this.value == 'Message') { this.value = ''; }" onblur="if(this.value == '') { this.value = 'Message'; }" value="Message">Message</textarea>
-		<a href="#" class="btn-image" onclick="$('#comments-form').submit(); return false;">Post this comment</a>
-    </form>          
+        <textarea cols="88" rows="12" name="comment" value="Comment" onFocus="if(this.value == 'Comment') {this.value = '';}" onBlur="if (this.value == '') {this.value = 'Comment';}"></textarea>
+		<a href="#" class="btn-image" onclick="jQuery('#comments-form').submit(); return false;">Post this comment</a>
+    </form>
 	<?php endif; ?>
     <!-- /Comments Form -->
-	
+
 </div>
 <!-- /Comments -->
